eb9ca0ea80cbd3e2cade829c8365dc1603d78876
Steven <dev@stevenm.co>
2026-02-14 12:00:32 +1100

    Add sql to the autocmd that makes indentation 2 spaces

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -22,7 +22,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "gleam",
+	pattern = { "gleam", "sql" },
 	callback = function()
 		vim.opt_local.shiftwidth = 2
 		vim.opt_local.tabstop = 2
